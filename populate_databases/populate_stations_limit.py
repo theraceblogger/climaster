eda6f2a2c6e2bd0a05ba43b14b04d89df3fede49
Chuck Schultz <theraceblogger@comcast.net>
2021-07-26 10:35:06 -0600

    update populate stations limit

--- populate_databases/populate_stations_limit.py
+++ populate_databases/populate_stations_limit.py
@@ -96,7 +96,7 @@ results = json.loads(j)
 
 for result in results:
     try:
-        insert_sql = "INSERT INTO weather.stations_raw (station_id, station_jsonb) VALUES (%s,%s) ON CONFLICT (station_id) DO UPDATE SET station_jsonb = %s"
+        insert_sql = "INSERT INTO weather.stations_raw_limit (station_id, station_jsonb) VALUES (%s,%s) ON CONFLICT (station_id) DO UPDATE SET station_jsonb = %s"
         cur.execute(insert_sql, (result['id'], json.dumps(result, indent=4, sort_keys=True), json.dumps(result, indent=4, sort_keys=True))) 
     except:
         print ('could not iterate through results')
