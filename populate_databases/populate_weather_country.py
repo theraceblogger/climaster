f3212824a046194b6edf29d568452cc907af5778
Chuck Schultz <theraceblogger@comcast.net>
2021-07-31 23:30:30 -0600

    update populate weather by country

--- populate_databases/populate_weather_country.py
+++ populate_databases/populate_weather_country.py
@@ -46,21 +46,18 @@ def load_data(url, country, off_set=1):
         r = requests.get(url2, headers=header)
         j = r.json()
         for result in j['results']:
-            insert_sql = f"INSERT INTO weather.{country} (station_id, date, data_type, value, attributes) VALUES (%s,%s,%s,%s,%s) ON CONFLICT (station_id, date, data_type) DO UPDATE SET value = %s, attributes = %s"
-            cur.execute(insert_sql, (result['station'], result['date'], result['datatype'], result['value'], result['attributes'], result['value'], result['attributes']))
-            # try:
-            #     insert_sql = "INSERT INTO weather.%s (station_id, date, data_type, value, attributes) VALUES (%s,%s,%s,%s,%s) ON CONFLICT (station_id, date, data_type) DO UPDATE SET value = %s, attributes = %s"
-            #     cur.execute(insert_sql, (country, result['station'], result['date'], result['datatype'], result['value'], result['attributes'], result['value'], result['attributes']))
-            # except:
-            #     print ('could not iterate through results')
+            # insert_sql = f"INSERT INTO weather.{country} (station_id, date, datatype, value, attributes) VALUES (%s,%s,%s,%s,%s) ON CONFLICT (station_id, date, data_type) DO UPDATE SET value = %s, attributes = %s"
+            # cur.execute(insert_sql, (result['station'], result['date'], result['datatype'], result['value'], result['attributes'], result['value'], result['attributes']))
+            try:
+                insert_sql = "INSERT INTO weather.%s (station_id, date, data_type, value, attributes) VALUES (%s,%s,%s,%s,%s) ON CONFLICT (station_id, date, data_type) DO UPDATE SET value = %s, attributes = %s"
+                cur.execute(insert_sql, (country, result['station'], result['date'], result['datatype'], result['value'], result['attributes'], result['value'], result['attributes']))
+            except:
+                print ('could not iterate through results')
         off_set += 1000
         if (off_set <= j['metadata']['resultset']['count']):
             load_data(url, country, off_set)
     except KeyError:
         pass
-    # except Exception as error:
-    #     print(error)
-    #     pass
 
         
 # Function gets data by customizing the iterations from the metadata and calling load_data()
@@ -115,7 +112,7 @@ results = cur.fetchall()
 stations_loaded = {}
 for result in results:  # for country in table (entire row)
     stations_loaded[result[0]] = 0  # initialize dictionary entry
-    # create_table(result[0])  # create table for country
+    create_table(result[0])  # create table for country
     for station in result[1]:  # for station in country
         get_data(station, result[0])  # get station data and load
     stations_loaded[result[0]] = stations_loaded[result[0]] + 1
