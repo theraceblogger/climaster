f0baffd4ab6aacf068c3b69897add91303f5ef87
Chuck Schultz <theraceblogger@comcast.net>
2021-08-03 19:29:29 -0600

    populate weather

--- populate_databases/populate_weather.py
+++ populate_databases/populate_weather.py
@@ -1,11 +1,10 @@
-## This script gets data from NOAA, and stores it in weather.noaa_raw
+## This script gets data from NOAA, and stores it in weather_raw
 import os
 import psycopg2
 from psycopg2.extras import DictCursor
 import requests
 import json
 import time
-import pandas as pd
 
 
 # Function that connects to database
@@ -34,45 +33,43 @@ noaa_token = os.environ['noaa_token']
 header = {'token': noaa_token}
 base_url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data"
 dataset_id = "?datasetid=GHCND"
-datatype_id = "&datatypeid=TAVG&TMIN&TMAX&PRCP&SNOW"
+datatype_id = "&datatypeid="
+datatype = "TMIN,TMAX,PRCP,SNOW,SNWD"
 station_id = "&stationid="
 start_date = "&startdate="
 end_date = "&enddate="
+units = "&units=standard"
 limit = "&limit=1000"
 offset = "&offset="
 
 
-# Function gets weather station id, mindate and maxdate
-# Calls get_data() for each weather station's data
-def get_meta():
-    query = "SELECT srl.station_id, srl.station_jsonb ->> 'mindate', srl.station_jsonb ->> 'maxdate' FROM weather.stations_raw_limit srl"
-    cur.execute(query)
-    results = cur.fetchall()
-    for result in results:
-        get_data(result)
-
-
 # Function gets data by customizing the iterations from the metadata and calling load_data()
-def get_data(result): # result is a list of strings
-    station, start, end = result[0], result[1], result[2]
+def get_data(station):
+    query = f"SELECT sw.station_id, srl.station_jsonb ->> 'mindate', sw.station_jsonb ->> 'maxdate' FROM weather.stations_world sw WHERE sw.station_id = '{station}'"
+    cur.execute(query)
+    meta = cur.fetchall()
+    start, end = meta[0][0], meta[0][1]
     start_yr, end_yr = start[:4], end[:4]
+    if int(start_yr) < 1990:
+        start_yr = '1990'
+        start = '1990-01-01'
     num_years = int(end_yr) - int(start_yr) +1
 
     for year in range(num_years):
         if num_years == 1:
-            url = base_url + dataset_id + datatype_id + station_id + station + start_date + start + end_date + end + limit + offset
+            url = base_url + dataset_id + datatype_id + datatype + station_id + station + start_date + start + end_date + end + units + limit + offset
             load_data(url)
 
         elif year == 0:
-            url = base_url + dataset_id + datatype_id + station_id + station + start_date + start + end_date + start_yr + "-12-31" + limit + offset
+            url = base_url + dataset_id + datatype_id + datatype + station_id + station + start_date + start + end_date + start_yr + "-12-31" + units + limit + offset
             load_data(url)
 
         elif year == num_years - 1:
-            url = base_url + dataset_id + datatype_id + station_id + station + start_date + end_yr + "-01-01" + end_date + end + limit + offset
+            url = base_url + dataset_id + datatype_id + datatype + station_id + station + start_date + end_yr + "-01-01" + end_date + end + units + limit + offset
             load_data(url)
 
         else:
-            url = base_url + dataset_id + datatype_id + station_id + station + start_date + str(int(start_yr) + year) + "-01-01" + end_date + str(int(start_yr) + year) + "-12-31" + limit + offset
+            url = base_url + dataset_id + datatype_id + datatype + station_id + station + start_date + str(int(start_yr) + year) + "-01-01" + end_date + str(int(start_yr) + year) + "-12-31" + units + limit + offset
             load_data(url)
 
 
@@ -80,12 +77,12 @@ def get_data(result): # result is a list of strings
 def load_data(url, off_set=1):
     try:
         url2 = url + str(off_set)
-        time.sleep(1)
+        time.sleep(10)
         r = requests.get(url2, headers=header)
         j = r.json()
         for result in j['results']:
             try:
-                insert_sql = "INSERT INTO weather.noaa_raw (station_id, date, data_type, noaa_jsonb) VALUES (%s,%s,%s,%s) ON CONFLICT (station_id, date, data_type) DO UPDATE SET noaa_jsonb = %s"
+                insert_sql = "INSERT INTO weather.weather_raw (station_id, date, datatype, weather_jsonb) VALUES (%s,%s,%s,%s) ON CONFLICT (station_id, date, datatype) DO UPDATE SET weather_jsonb = %s"
                 cur.execute(insert_sql, (result['station'], result['date'], result['datatype'], json.dumps(result, indent=4, sort_keys=True), json.dumps(result, indent=4, sort_keys=True)))
             except:
                 print ('could not iterate through results')
@@ -96,4 +93,28 @@ def load_data(url, off_set=1):
         pass
 
 
-get_meta()
+# get list of stations
+query = "SELECT station_id FROM weather.stations_world"
+cur.execute(query)
+results = cur.fetchall()
+
+stations = []
+    for result in results:
+        stations.append(result[0])
+
+# get number of stations loaded for index
+query = "SELECT COUNT(*) FROM weather.stations_loaded"
+cur.execute(query)
+count = cur.fetchall()
+begin_index = count[0]
+
+# get weather data and load into table weather_raw
+for i in range(begin_index, len(stations)):
+    station = stations[i]
+    get_data(station)
+    # update stations_loaded
+    try:
+        insert_sql = "INSERT INTO weather.stations_loaded (station_id) VALUES (%s) ON CONFLICT (station_id) DO UPDATE SET station_id = %s"
+        cur.execute(insert_sql, (station, station))
+    except:
+        print ('could not update stations_loaded')
