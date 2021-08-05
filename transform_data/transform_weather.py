3d0ef9795b577b7e81b357605d4cb4ba8fab053d
Chuck Schultz <theraceblogger@comcast.net>
2021-08-04 19:08:58 -0600

    requirements and transform weather

--- transform_data/transform_weather.py
+++ transform_data/transform_weather.py
@@ -4,6 +4,7 @@ from psycopg2.extras import DictCursor
 import requests
 import json
 import pandas as pd
+from functools import reduce
 
 
 # Function that connects to database
@@ -26,3 +27,34 @@ def db_connect():
 
 cur = db_connect()
 
+fields = ['TMIN', 'TMAX', 'PRCP', 'SNOW', 'SNWD']
+for field in fields:
+    query = f"SELECT (wr.date)::date, AVG((wr.weather_jsonb ->> 'value')::decimal) {field}\
+        FROM weather.weather_raw wr\
+            WHERE wr.datatype = {field}\
+                GROUP BY wr.date\
+                    ORDER BY wr.date"
+    cur.execute(query)
+    results = cur.fetchall()
+
+    flat_results = []
+    for result in results:
+        flat_results.append(result[0])
+    field = pd.DataFrame(flat_results)
+
+df = reduce(lambda  left,right: pd.merge(left,right,on=['date'], how='outer'), fields)
+df['tavg'] = df[['tmin', 'tmax']].mean(axis=1)
+print(df.head(10))
+# j = df.to_json(orient='records')
+# results = json.loads(j)
+
+# for result in results:
+#     try:
+#         insert_sql = "INSERT INTO weather.weather_daily (date, tmin, tmax, tavg, prcp, snow, snwd)\
+#             VALUES (%s,%s,%s,%s,%s,%s,%s)\
+#                 ON CONFLICT (date)\
+#                     DO UPDATE SET tmin = %s, tmax = %s, tavg = %s, prcp = %s, snow = %s, snwd = %s"
+#         cur.execute(insert_sql, (result['date'], result['tmin'], result['tmax'], result['tavg'], result['prcp'], result['snow'],\
+#             result['snwd'], result['tmin'], result['tmax'], result['tavg'], result['prcp'], result['snow'], result['snwd']))
+#     except:
+#         print ('could not iterate through results')
