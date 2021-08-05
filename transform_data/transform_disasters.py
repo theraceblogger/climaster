d232d3fc51bca65ef242af41f766c1df1fa399b1
Chuck Schultz <theraceblogger@comcast.net>
2021-08-04 19:00:48 -0600

    transform disasters

--- transform_data/transform_disasters.py
+++ transform_data/transform_disasters.py
@@ -4,6 +4,8 @@ from psycopg2.extras import DictCursor
 import requests
 import json
 import pandas as pd
+import pycountry
+from pycountry_convert import country_alpha3_to_country_alpha2
 
 
 # Function that connects to database
@@ -25,3 +27,51 @@ def db_connect():
     return cur
 
 cur = db_connect()
+
+
+extras_dict = {
+    "SUN":"RU",
+    "YUG":"RS",
+    "DFR":"DE",
+    "SCG":"RS",
+    "CSK":"CZ",
+    "YMN":"YE",
+    "SPI":"ES",
+    "YMD":"YE",
+    "AZO":"PT",
+    "ANT":"CW",
+    "DDR":"DE"}
+
+def add_cc(df):
+    cc = []
+    for i in range(df.shape[0]):
+        try:
+            iso = country_alpha3_to_country_alpha2(df.iloc[i].ISO)
+            cc.append(iso)
+        except KeyError:
+            cc.append(extras_dict[df.iloc[i].ISO])
+    df['cc'] = cc
+    return df
+
+
+query = "SELECT dr.disaster_jsonb FROM weather.disaster_raw dr ORDER BY dr.disaster_jsonb ->> 'Year', dr.disaster_jsonb ->> 'Month', dr.disaster_jsonb ->> 'Day'"
+cur.execute(query)
+results = cur.fetchall()
+
+flat_results = []
+for result in results:
+    flat_results.append(result[0])
+df = pd.DataFrame(flat_results)
+
+df = add_cc(df)
+
+j = df.to_json(orient='records')
+results = json.loads(j)
+
+for result in results:
+    try:
+        insert_sql = "INSERT INTO weather.disasters_clean (disaster_no, year, country_code, region, disaster_type, deaths, damages)\
+            VALUES (%s,%s,%s,%s,%s,%s,%s) ON CONFLICT (disaster_no) DO UPDATE SET year = %s, country_code = %s, region = %s, disaster_type = %s, deaths = %s, damages = %s"
+        cur.execute(insert_sql, (result['Dis No'], result['Year'], result['cc'], result['Region'], result['Disaster Type'], result['Total Deaths'], result['Total Damages (\'000 US$)'], result['Year'], result['cc'], result['Region'], result['Disaster Type'], result['Total Deaths'], result['Total Damages (\'000 US$)']))
+    except:
+        print ('could not iterate through results')
