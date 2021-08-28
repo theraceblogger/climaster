e469cae3bed06adcbd046ab72224aa3ac9c4eca6
Chuck Schultz <theraceblogger@comcast.net>
2021-08-28 16:58:25 -0600

    populate tmin

--- populate_databases/populate_tmin.py
+++ populate_databases/populate_tmin.py
@@ -44,7 +44,7 @@ limit = "&limit=1000"
 offset = "&offset="
 
 # Function gets data by customizing the iterations from the metadata (years:1950+) and calling load_data()
-def get_data(station):
+def get_data(station, api_calls):
     query = f"SELECT sl.station_jsonb ->> 'mindate', sl.station_jsonb ->> 'maxdate', sl.country_code, sl.region FROM weather.stations_to_load sl WHERE sl.station_id = '{station}'"
     cur.execute(query)
     meta = cur.fetchall()
@@ -61,22 +61,23 @@ def get_data(station):
     for year in range(num_years):
         if num_years == 1:
             url = base_url + dataset_id + datatype_id + datatype + station_id + station + start_date + start + end_date + end + units + limit + offset
-            load_data(url, country, region)
+            api_calls = load_data(url, country, region, api_calls)
 
         elif year == 0:
             url = base_url + dataset_id + datatype_id + datatype + station_id + station + start_date + start + end_date + start_yr + "-12-31" + units + limit + offset
-            load_data(url, country, region)
+            api_calls = load_data(url, country, region, api_calls)
 
         elif year == num_years - 1:
             url = base_url + dataset_id + datatype_id + datatype + station_id + station + start_date + end_yr + "-01-01" + end_date + end + units + limit + offset
-            load_data(url, country, region)
+            api_calls = load_data(url, country, region, api_calls)
 
         else:
             url = base_url + dataset_id + datatype_id + datatype + station_id + station + start_date + str(int(start_yr) + year) + "-01-01" + end_date + str(int(start_yr) + year) + "-12-31" + units + limit + offset
-            load_data(url, country, region)
+            api_calls = load_data(url, country, region, api_calls)
+    return api_calls
 
 # Function gets the data and inserts it into weather_tmin, 1000 at a time
-def load_data(url, country, region, off_set=1):
+def load_data(url, country, region, api_calls, off_set=1):
     try:
         url2 = url + str(off_set)
         time.sleep(.2)
@@ -94,7 +95,7 @@ def load_data(url, country, region, off_set=1):
                 
             off_set += 1000
             if (off_set <= j['metadata']['resultset']['count']):
-                load_data(url, country, region, off_set)
+                load_data(url, country, region, api_calls, off_set)
         elif r.status_code == 429:
             sys.exit(f'Too many API calls!\n{api_calls} requests')
         else:
@@ -102,6 +103,7 @@ def load_data(url, country, region, off_set=1):
             print(url2)
     except KeyError:
         pass
+    return api_calls
 
 # Main function - calls get_data() for stations_to_load and updates stations_loaded (Must manually TRUNCATE)
 def get_weather():
@@ -129,7 +131,7 @@ def get_weather():
         if station in stations_loaded:
             continue
         else:
-            get_data(station)
+            api_calls = get_data(station, api_calls)
             # update stations_loaded
             try:
                 insert_sql = "INSERT INTO weather.stations_loaded (station_id) VALUES (%s) ON CONFLICT (station_id) DO UPDATE SET station_id = %s"
