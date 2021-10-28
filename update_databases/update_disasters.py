a0dae515a77f7e5446aea534027841452fa17966
Chuck Schultz <theraceblogger@comcast.net>
2021-10-28 12:26:38 -0600

    change timedelta to 14 days

--- update_databases/update_disasters.py
+++ update_databases/update_disasters.py
@@ -107,7 +107,7 @@ oceania = polynesia + australia_new_zealand + melanesia + micronesia
 
 # set date variables
 end = date.today()
-start = end - timedelta(days=60)
+start = end - timedelta(days=14)
 end = int(end.strftime("%Y"))
 start = int(start.strftime("%Y"))
 
